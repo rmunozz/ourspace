30b06b52e37b9a2bdd51558b3e0e44be8dd42af3
nick <ndichev@ucsc.edu>
2016-10-19 21:23:17 -0700

    git fixes

--- controllers/default.py
+++ controllers/default.py
@@ -8,17 +8,35 @@
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 
+def get_user_name_from_email(email):
+    """Returns a string corresponding to the user first and last names,
+    given the user email."""
+    u = db(db.auth_user.email == email).select().first()
+    if u is None:
+        return 'None'
+    else:
+        return ' '.join([u.first_name, u.last_name])
+
 
 def index():
     """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
+    This is your main controller.
     """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    # I am creating a bogus list here, just to have some divs appear in the
+    # view.  You need to read at most 20 posts from the database, in order of
+    # most recent first, and you need to return that list here.
+    # Note that posts is NOT a list of strings in your actual code; it is
+    # what you get from a db(...).select(...).
+    posts = ['banana', 'pear', 'eggplant']
+    return dict(posts=posts)
+
+
+@auth.requires_login()
+def edit():
+    """
+    This is the page to create / edit / delete a post.
+    """
+    return dict()
 
 
 def user():
