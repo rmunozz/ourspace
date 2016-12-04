ac0d2fed5fc73ffbcd31110409500e80ba1c3b06
nick <ndichev@ucsc.edu>
2016-12-03 17:36:57 -0800

    basic styling with bootstrap

--- models/tables.py
+++ models/tables.py
@@ -9,13 +9,6 @@
 
 import datetime
 
-db.define_table('post',
-                Field('user_email', default=auth.user.email if auth.user_id else None),
-                Field('post_content', 'text'),
-                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
-                )
-
 db.define_table('folder',
                 Field('user_email', default=auth.user.email if auth.user_id else None),
                 Field('folder_name', 'text'),
@@ -23,10 +16,9 @@ db.define_table('folder',
                 )
 
 # I don't want to display the user email by default in all forms.
-db.post.user_email.readable = db.post.user_email.writable = False
-db.post.post_content.requires = IS_NOT_EMPTY()
-db.post.created_on.readable = db.post.created_on.writable = False
-db.post.updated_on.readable = db.post.updated_on.writable = False
+db.folder.user_email.readable = db.folder.user_email.writable = False
+db.folder.folder_name.requires = IS_NOT_EMPTY()
+db.folder.url_content.requires = IS_NOT_EMPTY()
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
